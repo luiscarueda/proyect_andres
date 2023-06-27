39c380c834aee8aaea2120c44151da9ab26284b7
luiscarlosrueda <luiscrueda.ing@gmail.com>
2023-06-27 16:47:20 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> IT IS A MODIFICATION TO PROBE THE DEPLOY.
         </p>
         <a
           className="App-link"
