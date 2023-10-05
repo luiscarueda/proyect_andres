43afb6b0844f183462e1734db7fde9c162aeb81d
luiscarlosrueda <luiscrueda.ing@gmail.com>
2023-10-04 20:47:25 -0500

    redes sociales

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import './contact.css'
-
+import {BsFacebook,BsInstagram,BsWhatsapp } from 'react-icons/bs'
 const Contact = () => {
   return (
-    <div className='  cta initial section__padding' id='CONTACT'>
-      <div className="gpt3__cta-content">
+    <section className='cta  section__padding' id='CONTACT'>
+      <div className="cta__content">
         <p>quieres conocer mas de mi academia?</p>
         <h3>SIGUEME EN MIS REDES SOCIALES.</h3>
       </div>
-     <div className="cta__btn">
-      <button type="button">Get Started</button>
+     <div className='cta__icons'>
+     <a href='https://www.facebook.com/borealislatindance?locale=es_LA' target='_blank' rel="noreferrer">
+          <BsFacebook fontSize={40}/>
+     </a>
+     <a href='https://www.instagram.com/borealislatindance/' target='_blank' rel="noreferrer">
+      <BsInstagram fontSize={40}/>
+     </a>
+     <a href='https://wa.me/573006179301?text=Hola quiero informacion de su negocio' target= '_blank' rel="noreferrer">
+      <BsWhatsapp fontSize={40} />
+     </a>
+     
+
      </div>
-    </div>
+     
+    </section>
   )
 }
 
